9f80bf5460c62c31259e4223c341b3f9c004b1e4
Nadia Gill <nadia.gill2006@gmail.com>
2025-04-07 15:38:23 +0200

    custom files - image and title

--- src/Desktop/Components/File/File.tsx
+++ src/Desktop/Components/File/File.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./File.scss";
 
-interface FileProps{
-    isGrid: boolean
+interface FileProps {
+    id: number,
+    isGrid: boolean,
+    image: string,
+    title: string
 }
 
-const File: React.FC<FileProps> = ({isGrid}) => {
+
+export default function File({isGrid, id, image, title}: FileProps){
     const[offset, setOffset] = useState<Array<number>>([0,0])
-    const[xy, setxy] = useState<Array<number>>([0,0])
+    const[xy, setxy] = useState<Array<number>>([0,(100*id-100)])
+    const[isActive, setActive] = useState<boolean>(false);
     const[styleOfMovingFile,setStyleOfMovingFile] = useState({ transform: `translate(${(xy[0]-offset[0])}px, ${(xy[1]-offset[1])}px)`, opacity: '1' })
-    
+    const drag = new Image(0,0);
+    const divRef = useRef<HTMLDivElement>(null);
+    drag.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
+    useEffect(() => {
+        const handleClick = (e: MouseEvent) => {
+          if (divRef.current && !divRef.current.contains(e.target as Node)) {
+            setActive(false); // kliknięcie poza boxem
+          }
+        };
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
+    }, []);
+
     //offset of the mouse relative to the object
     const getOffset = (e:React.DragEvent) => {
         let offsetX = e.nativeEvent.offsetX;
@@ -23,7 +41,7 @@ const File: React.FC<FileProps> = ({isGrid}) => {
          let x = e.clientX;
          let y = e.clientY;
          setxy(() =>[x,y]);
-         setStyleOfMovingFile({ transform: `translate(${(x-offset[0])}px, ${(y-offset[1])}px)`, opacity: `${isGrid ? '0':'1'}` })
+         setStyleOfMovingFile({...styleOfMovingFile, transform: `translate(${(x-offset[0])}px, ${(y-offset[1])}px)`, opacity: `${isGrid ? '0.5':'1'}` })
     }
 
     // grid-like placement
@@ -31,23 +49,27 @@ const File: React.FC<FileProps> = ({isGrid}) => {
         let x = Math.round((e.clientX-offset[0])/100)*100;
         let y = Math.round((e.clientY-offset[1])/100)*100;
         setxy(() =>[x,y]);
-        setStyleOfMovingFile({ transform: `translate(${x}px, ${y}px)`, opacity: '1' })
+        setStyleOfMovingFile({...styleOfMovingFile, transform: `translate(${x}px, ${y}px)`, opacity: '1' })
     }
 
     return(
         <>
          <div 
             style={styleOfMovingFile} 
-            className="file" 
+            ref={divRef}
+            className={`file ${isActive ? "file-active":''}`} 
             draggable="true" 
+            onClick={() => setActive(true)}
+            onBlur={() => setActive(false)}
             onDragEnter={(e) => e.preventDefault()} 
             onDragOver={(e) => {e.dataTransfer.dropEffect = "move";e.preventDefault()}} 
-            onDragStart={(e) => {getOffset(e)}} 
+            onDragStart={(e) => {setActive(true);e.dataTransfer.setDragImage(drag, 0, 0);getOffset(e)}} 
             onDrag={(e) => handleDrag(e)}
-            onDragEnd={(e) => {isGrid ? finalPlace(e) : handleDrag(e)}}>
+            onDragEnd={(e) => {isGrid ? finalPlace(e) : handleDrag(e)}}
+        >
+            <img src={image} alt={title}/>
+            <p className='title'>{title}</p>
          </div>
         </>
     );
 }
-
-export default File;
--- src/Desktop/Desktop.tsx
+++ src/Desktop/Desktop.tsx
@@ -2,13 +2,30 @@ import React, { useState } from 'react';
 import File from './Components/File/File';
 import './Desktop.scss';
 
+
+const fileData = [
+  {
+    id: 1,
+    title: "Pscółka",
+    image: "images/bzz.png"
+  },
+  {
+    id: 2,
+    title: "Pscółkaa",
+    image: "images/bzz.png"
+  },
+];
+
 function Desktop() {
   const[isGrid, setIsGrid] = useState<boolean>(true);
   
   return (
     <>
     <div className="main" >
-      <File  isGrid={isGrid} />
+      {fileData.map((file) => (
+        <File isGrid={isGrid} id={file.id} image={file.image} title={file.title} key={file.id}/>
+      ))}
+      
     </div>
     </>
   );
