e06e6ddddba0e48b962628aa8745fc6645e91ff7
Nadia Gill <nadia.gill2006@gmail.com>
2025-05-01 19:48:09 +0200

    moved folder data to json

--- src/Desktop/Desktop.tsx
+++ src/Desktop/Desktop.tsx
@@ -2,70 +2,10 @@ import React, { useState } from 'react';
 import File from './Components/File/File';
 import './Desktop.scss';
 import Background from '../Assets/Images/background2.jpg';
+import data from '../Assets/Data/data.json';
 
 
-const folderData = [
-  {
-    id: 1,
-    type: 'folder',
-    source: "images/bzz.png",
-    title: "Pscólka",
-    content: [
-      {
-        id: 1,
-        type: "folder",
-        source: "images/bzz.png",
-        title: "folder",
-        content: []
-      },
-      {
-        id: 2,
-        type: "image",
-        source: "images/bzz.png",
-        title: "zdjecie :>",
-        content: []
-      },
-      {
-        id: 3,
-        type: "image",
-        source: "images/bzz.png",
-        title: "zdjecie 2 :>",
-        content: []
-      },
-      {
-        id: 4,
-        type: "image",
-        source: "images/bzz.png",
-        title: "zdjecie 3 >:)",
-        content: []
-      },
-      {
-        id: 5,
-        type: "image",
-        source: "images/bzz.png",
-        title: "przyklad row",
-        content: []
-      },
-    ]
-  },
-  {
-    id: 2,
-    type: 'folder',
-    title: "Pscólkaa",
-    source: "images/bzz.png",
-    content: []
-  },
-];
-
-const fileData = [
-  {
-    id: 1,
-    type: "folder",
-    source: "images/bzz.png",
-    title: "folder",
-    content: []
-  }
-]
+const folderData = data.files;
 
 function Desktop() {
   const[isGrid, setIsGrid] = useState<boolean>(true);
