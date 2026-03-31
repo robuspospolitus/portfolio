2ca74b942494f8adaae88a1d705015d92e1564a9
Nadia Gill <nadia.gill2006@gmail.com>
2026-03-31 14:05:57 +0200

    feat: home pixel corners

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import HomeTab from './Tabs/HomeTab/HomeTab';
 import ProjectsTab from './Tabs/ProjectsTab/ProjectsTab';
 import MoreTab from './Tabs/MoreTab/MoreTab';
 import Background from '../../Assets/Images/image.png';
+import '../../styles/pixel-corners.scss';
 import './Home.scss';
 
 export default function Home() {
@@ -14,11 +15,11 @@ export default function Home() {
             <img className='home-background' src={Background} draggable={false} alt='background' onContextMenu={e => {e.preventDefault()}}/>
             <div id="home">
                 <nav id='home-tabs'>
-                    <div className={`home-tab ${activeTab===1 && "active-tab"}`} onClick={()=>setActiveTab(1)}>Home</div>
-                    <div className={`home-tab ${activeTab===2 && "active-tab"}`} onClick={()=>setActiveTab(2)}>Projects</div>
-                    <div className={`home-tab ${activeTab===3 && "active-tab"}`} onClick={()=>setActiveTab(3)}>More</div>
+                    <div className={`home-tab ${activeTab===1 ? "active-tab pixel-corners-top":"pixel-corners"}`} onClick={()=>setActiveTab(1)}>Home</div>
+                    <div className={`home-tab ${activeTab===2 ? "active-tab pixel-corners-top":"pixel-corners"}`} onClick={()=>setActiveTab(2)}>Projects</div>
+                    <div className={`home-tab ${activeTab===3 ? "active-tab pixel-corners-top":"pixel-corners"}`} onClick={()=>setActiveTab(3)}>More</div>
                </nav>
-                <main id='home-content'>
+                <main id='home-content' className={activeTab===1 ? "pixel-corners-top-right":`pixel-corners-top`} style={{borderTopLeftRadius: activeTab===1 ?"0":"24px"}}>
                     {activeTab===1 && <HomeTab/>}
                     {activeTab===2 && <ProjectsTab/>}
                     {activeTab===3 && <MoreTab/>}
